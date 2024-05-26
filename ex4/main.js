1037d3a112d9aca773190550f28eb8f6edbdbf9e
kK4ssio <diasmonteirokassio@gmail.com>
2024-05-26 02:29:28 -0300

    README

---
+++ ex4/main.js
@@ -0,0 +1,11 @@
+// EXERCICIO #4
+// uma função que calcula a altura e a largura de um retângulo digitados em input, e que entrega a area total.
+
+function AreaTotal(){
+var com = document.getElementById("Comprimento").value;
+var larg = document.getElementById("Largura").value;
+
+var area = com * larg;
+alert("Area total: "+area)
+
+}
