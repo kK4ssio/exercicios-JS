d4d54e3e8a9983aa2c4dbbe255a559b9445656fc
kK4ssio <diasmonteirokassio@gmail.com>
2024-05-26 02:23:56 -0300

    README

---
+++ ex1/main.js
@@ -0,0 +1,7 @@
+// EXERCICIO #1 
+// desenvolver uma função que quando o usuário insira o seu nome em um input, gere uma saudação com seu nome
+
+function Saudar(){
+    var nome = document.getElementById("NomeDigitado").value;
+alert("Olá, Saudações "+nome);
+}
